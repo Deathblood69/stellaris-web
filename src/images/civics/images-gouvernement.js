59d628a598fca5a0420ae06b3eb4ef2fa12e6ad3
Paul Essel <44263095+PaulEsl@users.noreply.github.com>
2022-11-25 19:14:45 +0100

    ajout image civic script

---
+++ src/images/civics/images-gouvernement.js
@@ -0,0 +1,266 @@
+import civic_agrarian_idyll from './civic_agrarian_idyll.png'
+import civic_ancient_caches_of_technology from './civic_ancient_caches_of_technology.png'
+import civic_anglers from './civic_anglers.png'
+import civic_aristocratic_elite from './civic_aristocratic_elite.png'
+import civic_ascensionists from './civic_ascensionists.png'
+import civic_awakened_machine from './civic_awakened_machine.png'
+import civic_barbaric_despoilers from './civic_barbaric_despoilers.png'
+import civic_beacon_of_liberty from './civic_beacon_of_liberty.png'
+import civic_brand_loyalty from './civic_brand_loyalty.png'
+import civic_byzantine_bureaucracy from './civic_byzantine_bureaucracy.png'
+import civic_caravaneer_caravansary from './civic_caravaneer_caravansary.png'
+import civic_caravaneer_fleet1 from './civic_caravaneer_fleet1.png'
+import civic_caravaneer_fleet2 from './civic_caravaneer_fleet2.png'
+import civic_caravaneer_fleet3 from './civic_caravaneer_fleet3.png'
+import civic_carrion_eaters from './civic_carrion_eaters.png'
+import civic_catalytic_processing from './civic_catalytic_processing.png'
+import civic_citizen_service from './civic_citizen_service.png'
+import civic_corp_pleasure_seekers from './civic_corp_pleasure_seekers.png'
+import civic_corporate_anglers from './civic_corporate_anglers.png'
+import civic_corporate_ascensionists from './civic_corporate_ascensionists.png'
+import civic_corporate_catalytic_processing from './civic_corporate_catalytic_processing.png'
+import civic_corporate_crafters from './civic_corporate_crafters.png'
+import civic_corporate_dominion from './civic_corporate_dominion.png'
+import civic_corporate_hedonism from './civic_corporate_hedonism.png'
+import civic_corporate_relentless_industrialists from './civic_corporate_relentless_industrialists.png'
+import civic_corporate_scavengers from './civic_corporate_scavengers.png'
+import civic_corporate_toxic_baths from './civic_corporate_toxic_baths.png'
+import civic_corvee_system from './civic_corvee_system.png'
+import civic_crafters from './civic_crafters.png'
+import civic_criminal_heritage from './civic_criminal_heritage.png'
+import civic_custodian_directives from './civic_custodian_directives.png'
+import civic_cutthroat_politics from './civic_cutthroat_politics.png'
+import civic_death_cult from './civic_death_cult.png'
+import civic_death_cult_corporate from './civic_death_cult_corporate.png'
+import civic_diplomatic_corps from './civic_diplomatic_corps.png'
+import civic_distinguished_admiralty from './civic_distinguished_admiralty.png'
+import civic_efficient_bureaucracy from './civic_efficient_bureaucracy.png'
+import civic_empire_in_decline from './civic_empire_in_decline.png'
+import civic_environmentalist from './civic_environmentalist.png'
+import civic_exalted_priesthood from './civic_exalted_priesthood.png'
+import civic_fanatic_purifiers from './civic_fanatic_purifiers.png'
+import civic_feudal_realm from './civic_feudal_realm.png'
+import civic_final_defense_protocols from './civic_final_defense_protocols.png'
+import civic_franchising from './civic_franchising.png'
+import civic_free_haven from './civic_free_haven.png'
+import civic_free_traders from './civic_free_traders.png'
+import civic_functional_architecture from './civic_functional_architecture.png'
+import civic_galactic_sovereign from './civic_galactic_sovereign.png'
+import civic_gospel_of_the_masses from './civic_gospel_of_the_masses.png'
+import civic_hive_ascensionists from './civic_hive_ascensionists.png'
+import civic_hive_ascetic from './civic_hive_ascetic.png'
+import civic_hive_catalytic_processing from './civic_hive_catalytic_processing.png'
+import civic_hive_cordyceptic_drones from './civic_hive_cordyceptic_drones.png'
+import civic_hive_devouring_swarm from './civic_hive_devouring_swarm.png'
+import civic_hive_divided_attention from './civic_hive_divided_attention.png'
+import civic_hive_empath from './civic_hive_empath.png'
+import civic_hive_idyllic_bloom from './civic_hive_idyllic_bloom.png'
+import civic_hive_life_seeded from './civic_hive_life_seeded.png'
+import civic_hive_memorialist from './civic_hive_memorialist.png'
+import civic_hive_natural_neural_network from './civic_hive_natural_neural_network.png'
+import civic_hive_one_mind from './civic_hive_one_mind.png'
+import civic_hive_pooled_knowledge from './civic_hive_pooled_knowledge.png'
+import civic_hive_strength_of_legions from './civic_hive_strength_of_legions.png'
+import civic_hive_subspace_ephapse from './civic_hive_subspace_ephapse.png'
+import civic_hive_subsumed_will from './civic_hive_subsumed_will.png'
+import civic_hive_toxic_baths from './civic_hive_toxic_baths.png'
+import civic_idealistic_foundation from './civic_idealistic_foundation.png'
+import civic_idyllic_bloom from './civic_idyllic_bloom.png'
+import civic_imperial_cult from './civic_imperial_cult.png'
+import civic_indentured_assets from './civic_indentured_assets.png'
+import civic_inwards_perfection from './civic_inwards_perfection.png'
+import civic_legal_counsel_civic from './civic_legal_counsel_civic.png'
+import civic_lethargic_leadership from './civic_lethargic_leadership.png'
+import civic_life_seeded from './civic_life_seeded.png'
+import civic_machine_ascensionists from './civic_machine_ascensionists.png'
+import civic_machine_assimilator from './civic_machine_assimilator.png'
+import civic_machine_builder from './civic_machine_builder.png'
+import civic_machine_built_to_last from './civic_machine_built_to_last.png'
+import civic_machine_catalytic_processing from './civic_machine_catalytic_processing.png'
+import civic_machine_delegated_functions from './civic_machine_delegated_functions.png'
+import civic_machine_factory_overclock from './civic_machine_factory_overclock.png'
+import civic_machine_introspective from './civic_machine_introspective.png'
+import civic_machine_maintenance_protocols from './civic_machine_maintenance_protocols.png'
+import civic_machine_memorialist from './civic_machine_memorialist.png'
+import civic_machine_ota_updates from './civic_machine_ota_updates.png'
+import civic_machine_predictive_analysis from './civic_machine_predictive_analysis.png'
+import civic_machine_remnants from './civic_machine_remnants.png'
+import civic_machine_replication from './civic_machine_replication.png'
+import civic_machine_rockbreakers from './civic_machine_rockbreakers.png'
+import civic_machine_servitor from './civic_machine_servitor.png'
+import civic_machine_terminator from './civic_machine_terminator.png'
+import civic_machine_toxic_baths from './civic_machine_toxic_baths.png'
+import civic_machine_unitary_cohesion from './civic_machine_unitary_cohesion.png'
+import civic_machine_warbots from './civic_machine_warbots.png'
+import civic_machine_zero_waste_protocols from './civic_machine_zero_waste_protocols.png'
+import civic_malfunctioning_custodian from './civic_malfunctioning_custodian.png'
+import civic_mechanists from './civic_mechanists.png'
+import civic_media_conglomerate from './civic_media_conglomerate.png'
+import civic_memorialist from './civic_memorialist.png'
+import civic_mercenaries from './civic_mercenaries.png'
+import civic_merchant_guilds from './civic_merchant_guilds.png'
+import civic_meritocracy from './civic_meritocracy.png'
+import civic_mining_guilds from './civic_mining_guilds.png'
+import civic_nationalistic_zeal from './civic_nationalistic_zeal.png'
+import civic_naval_contractors from './civic_naval_contractors.png'
+import civic_parliamentary_system from './civic_parliamentary_system.png'
+import civic_permanent_employment from './civic_permanent_employment.png'
+import civic_philosopher_king from './civic_philosopher_king.png'
+import civic_pleasure_seekers from './civic_pleasure_seekers.png'
+import civic_police_state from './civic_police_state.png'
+import civic_pompous_purists from './civic_pompous_purists.png'
+import civic_post_apocalyptic from './civic_post_apocalyptic.png'
+import civic_private_military_companies from './civic_private_military_companies.png'
+import civic_private_prospectors from './civic_private_prospectors.png'
+import civic_psionic_sovereign from './civic_psionic_sovereign.png'
+import civic_public_relations_specialists from './civic_public_relations_specialists.png'
+import civic_reanimated_armies from './civic_reanimated_armies.png'
+import civic_relentless_industrialists from './civic_relentless_industrialists.png'
+import civic_revanchist_fervor from './civic_revanchist_fervor.png'
+import civic_ruthless_competition from './civic_ruthless_competition.png'
+import civic_salvagers from './civic_salvagers.png'
+import civic_scavengers from './civic_scavengers.png'
+import civic_shadow_council from './civic_shadow_council.png'
+import civic_shared_burden from './civic_shared_burden.png'
+import civic_shroudwalkers from './civic_shroudwalkers.png'
+import civic_slaver_guilds from './civic_slaver_guilds.png'
+import civic_syncretic_evolution from './civic_syncretic_evolution.png'
+import civic_technocracy from './civic_technocracy.png'
+import civic_toxic_baths from './civic_toxic_baths.png'
+import civic_trading_posts from './civic_trading_posts.png'
+import civic_warrior_culture from './civic_warrior_culture.png'
+import intel_icon_unknown_civics from './intel_icon_unknown_civics.png'
+export const IMAGE_CIVIC = {
+    agrarian_idyll : civic_agrarian_idyll,
+    ancient_caches_of_technology: civic_ancient_caches_of_technology,
+    anglers : civic_anglers,
+    aristocratic_elite : civic_aristocratic_elite,
+    ascensionists : civic_ascensionists,
+    awakened_machine : civic_awakened_machine,
+    barbaric_despoilers: civic_barbaric_despoilers,
+    beacon_of_liberty: civic_beacon_of_liberty,
+    brand_loyalty: civic_brand_loyalty,
+    byzantine_bureaucracy: civic_byzantine_bureaucracy,
+    caravaneer_caravansary: civic_caravaneer_caravansary,
+    caravaneer_fleet1: civic_caravaneer_fleet1,
+    caravaneer_fleet2: civic_caravaneer_fleet2,
+    caravaneer_fleet3: civic_caravaneer_fleet3,
+    carrion_eaters: civic_carrion_eaters,
+    catalytic_processing: civic_catalytic_processing,
+    citizen_service: civic_citizen_service,
+    corp_pleasure_seekers: civic_corp_pleasure_seekers,
+    corporate_anglers: civic_corporate_anglers,
+    corporate_ascensionists: civic_corporate_ascensionists,
+    corporate_catalytic_processing: civic_corporate_catalytic_processing,
+    corporate_crafters: civic_corporate_crafters,
+    corporate_dominion: civic_corporate_dominion,
+    corporate_hedonism: civic_corporate_hedonism,
+    corporate_relentless_industrialists: civic_corporate_relentless_industrialists,
+    corporate_scavengers: civic_corporate_scavengers,
+    corporate_toxic_baths: civic_corporate_toxic_baths,
+    corvee_system: civic_corvee_system,
+    crafters: civic_crafters,
+    criminal_heritage: civic_criminal_heritage,
+    custodian_directives: civic_custodian_directives,
+    cutthroat_politics: civic_cutthroat_politics,
+    death_cult: civic_death_cult,
+    death_cult_corporate: civic_death_cult_corporate,
+    diplomatic_corps: civic_diplomatic_corps,
+    distinguished_admiralty: civic_distinguished_admiralty,
+    efficient_bureaucracy: civic_efficient_bureaucracy,
+    empire_in_decline: civic_empire_in_decline,
+    environmentalist: civic_environmentalist,
+    exalted_priesthood: civic_exalted_priesthood,
+    fanatic_purifiers: civic_fanatic_purifiers,
+    feudal_realm: civic_feudal_realm,
+    final_defense_protocols: civic_final_defense_protocols,
+    franchising: civic_franchising,
+    free_haven: civic_free_haven,
+    free_traders: civic_free_traders,
+    functional_architecture: civic_functional_architecture,
+    galactic_sovereign: civic_galactic_sovereign,
+    gospel_of_the_masses: civic_gospel_of_the_masses,
+    hive_ascensionists: civic_hive_ascensionists,
+    hive_ascetic: civic_hive_ascetic,
+    hive_catalytic_processing: civic_hive_catalytic_processing,
+    hive_cordyceptic_drones: civic_hive_cordyceptic_drones,
+    hive_devouring_swarm: civic_hive_devouring_swarm,
+    hive_divided_attention: civic_hive_divided_attention,
+    hive_empath: civic_hive_empath,
+    hive_idyllic_bloom: civic_hive_idyllic_bloom,
+    hive_life_seeded: civic_hive_life_seeded,
+    hive_memorialist: civic_hive_memorialist,
+    hive_natural_neural_network: civic_hive_natural_neural_network,
+    hive_one_mind: civic_hive_one_mind,
+    hive_pooled_knowledge: civic_hive_pooled_knowledge,
+    hive_strength_of_legions: civic_hive_strength_of_legions,
+    hive_subspace_ephapse: civic_hive_subspace_ephapse,
+    hive_subsumed_will: civic_hive_subsumed_will,
+    hive_toxic_baths: civic_hive_toxic_baths,
+    idealistic_foundation: civic_idealistic_foundation,
+    idyllic_bloom: civic_idyllic_bloom,
+    imperial_cult: civic_imperial_cult,
+    indentured_assets: civic_indentured_assets,
+    inwards_perfection: civic_inwards_perfection,
+    legal_counsel_civic: civic_legal_counsel_civic,
+    lethargic_leadership: civic_lethargic_leadership,
+    life_seeded: civic_life_seeded,
+    machine_ascensionists: civic_machine_ascensionists,
+    machine_assimilator: civic_machine_assimilator,
+    machine_builder: civic_machine_builder,
+    machine_built_to_last: civic_machine_built_to_last,
+    machine_catalytic_processing: civic_machine_catalytic_processing,
+    machine_delegated_functions: civic_machine_delegated_functions,
+    machine_factory_overclock: civic_machine_factory_overclock,
+    machine_introspective: civic_machine_introspective,
+    machine_maintenance_protocols: civic_machine_maintenance_protocols,
+    machine_memorialist: civic_machine_memorialist,
+    machine_ota_updates: civic_machine_ota_updates,
+    machine_predictive_analysis: civic_machine_predictive_analysis,
+    machine_remnants: civic_machine_remnants,
+    machine_replication: civic_machine_replication,
+    machine_rockbreakers: civic_machine_rockbreakers,
+    machine_servitor: civic_machine_servitor,
+    machine_terminator: civic_machine_terminator,
+    machine_toxic_baths: civic_machine_toxic_baths,
+    machine_unitary_cohesion: civic_machine_unitary_cohesion,
+    machine_warbots: civic_machine_warbots,
+    machine_zero_waste_protocols: civic_machine_zero_waste_protocols,
+    malfunctioning_custodia: civic_malfunctioning_custodian,
+    mechanists: civic_mechanists,
+    media_conglomerate: civic_media_conglomerate,
+    memorialist: civic_memorialist,
+    mercenaries: civic_mercenaries,
+    merchant_guilds: civic_merchant_guilds,
+    meritocracy: civic_meritocracy,
+    mining_guilds: civic_mining_guilds,
+    nationalistic_zeal: civic_nationalistic_zeal,
+    naval_contractors: civic_naval_contractors,
+    parliamentary_system: civic_parliamentary_system,
+    permanent_employment: civic_permanent_employment,
+    philosopher_king: civic_philosopher_king,
+    pleasure_seekers: civic_pleasure_seekers,
+    police_state: civic_police_state,
+    pompous_purists: civic_pompous_purists,
+    post_apocalyptic: civic_post_apocalyptic,
+    private_military_companies: civic_private_military_companies,
+    private_prospectors: civic_private_prospectors,
+    psionic_sovereign: civic_psionic_sovereign,
+    public_relations_specialists: civic_public_relations_specialists,
+    reanimated_armies: civic_reanimated_armies,
+    relentless_industrialists: civic_relentless_industrialists,
+    revanchist_fervor: civic_revanchist_fervor,
+    ruthless_competition: civic_ruthless_competition,
+    salvagers: civic_salvagers,
+    scavengers: civic_scavengers,
+    shadow_council: civic_shadow_council,
+    shared_burden: civic_shared_burden,
+    shroudwalkers: civic_shroudwalkers,
+    slaver_guilds: civic_slaver_guilds,
+    syncretic_evolution: civic_syncretic_evolution,
+    technocracy: civic_technocracy,
+    toxic_baths: civic_toxic_baths,
+    trading_posts: civic_trading_posts,
+    warrior_culture: civic_warrior_culture,
+    icon_unknown_civics: intel_icon_unknown_civics,
+}
